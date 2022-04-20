d8767d78a393af3342bd235557f2c660e80df56e
crono628 <84817352+crono628@users.noreply.github.com>
2022-04-20 17:58:17 -0400

    inventory update

--- src/components/data.js
+++ src/components/data.js
@@ -2,28 +2,28 @@ const data = [
   {
     name: 'Sony Betamax',
     src: 'https://www.extremetech.com/wp-content/uploads/2015/11/772066351852676897.jpg',
-    description: 'The Next Thing',
+    description: 'The future of home video, forever',
     price: 999,
     id: 1,
   },
   {
     name: 'Magnavox Camcorder',
     src: 'https://i.ebayimg.com/images/g/JcgAAOSwzI5fqssa/s-l400.jpg',
-    description: 'Smart. Very Smart',
+    description: 'Worth the neck ache',
     price: 1299,
     id: 2,
   },
   {
     name: 'Magnavox Giant Television',
     src: 'https://i.ebayimg.com/images/g/sHUAAOSwxsNfpp5q/s-l400.jpg',
-    description: 'Smart. Very Smart',
+    description: 'Guaranteed to occupy the most space in your living room',
     price: 1999,
     id: 3,
   },
   {
     name: 'Panasonic Stereo',
     src: 'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2015/11/1971-02001-626x834.jpg?resize=480:*',
-    description: 'By Panasonic',
+    description: '8-track tapes are totally reliable',
     price: 799,
     id: 4,
   },
@@ -33,6 +33,35 @@ const data = [
     description: 'VCR. WE LOVE INITIALS',
     price: 399,
     id: 5,
+  },
+  {
+    name: 'Nintendo Gameboy',
+    src: 'https://cdn.pocket-lint.com/r/s/660x/assets/images/149611-gadgets-feature-iconic-gadgets-of-the-90s-amazing-gadgets-and-gizmos-from-yesteryear-image8-m7zv6rd9xn-jpg.webp?v1',
+    description: 'Do you hate batteries AND your eyes? Play Gameboy!',
+    price: 199,
+    id: 6,
+  },
+  {
+    name: 'Sony Walkman',
+    src: 'https://i.insider.com/524c93b869bedd2663dc4091?width=1200&format=jpeg&auto=webp',
+    description: 'Listen to music anywhere AND look cool',
+    price: 149,
+    id: 7,
+  },
+  {
+    name: 'Casio Calculator Watch',
+    src: 'https://i.insider.com/524c79596bb3f7ec2fabf524?width=1200&format=jpeg&auto=webp',
+    description: 'Do simple math ANYWHERE',
+    price: 249,
+    id: 8,
+  },
+  {
+    name: 'The Clapper',
+    src: 'https://i.insider.com/524c6dffeab8ea2d4a54fc7c?width=1200&format=jpeg&auto=webp',
+    description:
+      'The future is now. Clap loudly and just enough times to control electricity.',
+    price: 29,
+    id: 9,
   },
 ];
 
