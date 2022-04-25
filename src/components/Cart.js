5bed859add0b88cf0b761c011dbe257f83cb19df
crono628 <84817352+crono628@users.noreply.github.com>
2022-04-25 12:14:08 -0400

    added subtotal, tax, shipping

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,28 +8,56 @@ const Cart = ({ items, onAdd, onRemove }) => {
         {item.name}
         <div className="cart-quantity">
           <button onClick={() => onRemove(item)}>-</button>
-          <div>
+          <div className="">
             {item.qty} x ${item.price}
           </div>
           <button onClick={() => onAdd(item)}>+</button>
         </div>
       </div>
-
-      <div> </div>
-      <div>${item.price * item.qty}</div>
     </div>
   ));
 
-  const totalPrice = items.reduce((a, b) => {
+  const subTotal = items.reduce((a, b) => {
     return a + b.price * b.qty;
   }, 0);
+
+  const tax = subTotal * 0.09;
+
+  const shippingCost = subTotal < 4000 ? 250 : 0;
+
+  const totalCost = shippingCost + tax + subTotal;
 
   return (
     <div className="cart-container">
       <h1>Cart</h1>
-      {items.length > 0 && <p>Cart Total: ${totalPrice}</p>}
+      {items.length > 0 && (
+        <div className="cart-confirm">
+          <div className="cart-math">
+            <table>
+              <thead>
+                <tr>
+                  <td>Subtotal:</td>
+                  <td>${subTotal.toFixed(2)}</td>
+                </tr>
+                <tr>
+                  <td>Tax:</td>
+                  <td>${tax.toFixed(2)}</td>
+                </tr>
+                <tr>
+                  <td>Shipping:</td>
+                  <td>${shippingCost.toFixed(2)}</td>
+                </tr>
+                <tr>
+                  <th>Total:</th>
+                  <th>${totalCost.toFixed(2)}</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div>{cartItems}</div>
+        </div>
+      )}
       {items.length === 0 && <div>Cart is Empty</div>}
-      {items.length > 0 && <div>{cartItems}</div>}
     </div>
   );
 };
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Welcome = () => {
   return (
     <img
+      alt="the Wiz"
       className="welcome"
       src="https://1.bp.blogspot.com/-cLCLe54gj7U/Xr7vQPbvsrI/AAAAAAAAFVs/AZMQq5-WbfwmGdHJmh-P2o3TQw0ugp7kQCLcBGAsYHQ/s1600/wiz%2Blogo.jpg"
     />
